e2ae6c6c5cbfff7d3c5e7be423e12a23224626d7
Gil32610 <gil.00000032610@unicap.br>
2022-10-11 17:33:06 +0000

    sum Utils

--- src/main/Utils.java
+++ src/main/Utils.java
@@ -1,17 +1,32 @@
 package src.main;
 
 public class Utils {
-    
-    public static SinglyLinkedList sum(SinglyLinkedList listAddendOne, SinglyLinkedList listAddendTwo){
-        
+
+    public static SinglyLinkedList sum(SinglyLinkedList listAddendOne, SinglyLinkedList listAddendTwo) {
+
         SinglyLinkedList sum = new SinglyLinkedList();
         SinglyListNode current1 = listAddendOne.getHead();
         SinglyListNode current2 = listAddendTwo.getHead();
         listAddendOne.reverse();
         listAddendTwo.reverse();
+        int carryOne = 0;
 
-        while(current1!= null || current2!= null){
+        while (current1 != null || current2 != null || carryOne > 0) {
+            int summing = carryOne;
 
+            if (current1 != null) {
+                summing += current1.getValue();
+                current1 = current1.getNext();
+            }
+            if (current2 != null) {
+                summing += current2.getValue();
+                current2 = current2.getNext();
+            }
+            int digit = summing % 10;
+            carryOne = summing / 10;
+            sum.addFirst(digit);
         }
+        sum.reverse();
+        return sum;
     }
 }
